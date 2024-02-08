e64f8a4f7247163be2ed2e1738513e3b143522f4
mr5499 <miikaravi99@gmail.com>
2024-02-08 17:40:07 +0200

    2.5 done

--- part2/course_information/src/App.jsx
+++ part2/course_information/src/App.jsx
@@ -1,39 +1,4 @@
-const Courses = ({ courses }) => {
-  return (
-    <div>
-      <h1>Web development curriculum</h1>
-      {courses.map(course => (
-        <div key={course.id}>
-          <Header name={course.name} />
-          <Content parts={course.parts} />
-        </div>
-      ))}
-    </div>
-  )
-}
-
-const Header = ({ name }) => {
-  return (
-    <h2>{name}</h2>
-  )
-}
-
-const Content = ({ parts }) => {
-  const sum = parts.reduce((total, part) => total + part.exercises, 0);
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part} />
-      ))}
-      <strong>total of {sum} exercises</strong>
-    </div>
-  )
-}
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
+import Courses from "./Course";
 
 const App = () => {
   const courses = [
