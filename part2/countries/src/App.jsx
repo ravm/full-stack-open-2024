0f2dccb52081f3c7530837a31d8797d278c95b00
mr5499 <miikaravi99@gmail.com>
2024-02-14 12:29:02 +0200

    working on 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,10 +1,47 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import countryService from './services/countries'
+
+const Countries = ({ countries, filter }) => {
+  return (
+    <div>
+      {countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()) || filter === '')
+      .map(country => <p key={country.cca3}>{country.name.common}</p>)}
+    </div>
+  )
+}
+
+const FilterCountries = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      find countries <input value={filter} onChange={handleFilterChange} />
+    </div>
+  )
+}
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    countryService
+      .getAll()
+      .then(initialCountries => {
+        setCountries(initialCountries)
+      })
+      .catch(err => {
+        console.log(`Error fetching countries: ${err}`)
+      })
+  }, [])
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value)
+  }
 
   return (
-    <>
-    </>              
+    <div>
+      <FilterCountries filter={filter} handleFilterChange={handleFilterChange} />
+      <Countries countries={countries} filter={filter} />
+    </div>
   )
 }
 
