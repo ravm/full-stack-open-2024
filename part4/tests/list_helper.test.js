d306177684f8c1a2d9e8311e7723a9e070c774f6
ravm <miika.ravi@sadeinnovations.com>
2024-09-24 13:56:15 +0300

    4.5

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -2,6 +2,41 @@ const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
+const listWithManyBlogs = [
+  {
+    _id: "5a422aa71b54a676234d17f1",
+    title: "The Strange Case of the Alchemist's Daughter",
+    author: "Theodora Goss",
+    url: "https://bookshop.org/p/books/the-strange-case-of-the-alchemist-s-daughter-volume-1-theodora-goss/217995?ean=9781481466516",
+    likes: 3,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f9",
+    title: "A Boy and His Dog at the End of the World",
+    author: "C. a. Fletcher",
+    url: "https://bookshop.org/p/books/a-boy-and-his-dog-at-the-end-of-the-world-c-a-fletcher/113361?ean=9780316449434",
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f2",
+    title: "American Gods",
+    author: "Neil Gaiman",
+    url: "https://bookshop.org/p/books/american-gods-neil-gaiman/6438874?ean=9780063081918",
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+    likes: 5,
+    __v: 0,
+  }
+];
+
 test("dummy returns one", () => {
   const blogs = [];
 
@@ -11,40 +46,6 @@ test("dummy returns one", () => {
 
 describe("total likes", () => {
   const emptyList = [];
-  const listWithManyBlogs = [
-    {
-      _id: "5a422aa71b54a676234d17f1",
-      title: "The Strange Case of the Alchemist's Daughter",
-      author: "Theodora Goss",
-      url: "https://bookshop.org/p/books/the-strange-case-of-the-alchemist-s-daughter-volume-1-theodora-goss/217995?ean=9781481466516",
-      likes: 3,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f9",
-      title: "A Boy and His Dog at the End of the World",
-      author: "C. a. Fletcher",
-      url: "https://bookshop.org/p/books/a-boy-and-his-dog-at-the-end-of-the-world-c-a-fletcher/113361?ean=9780316449434",
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f2",
-      title: "American Gods",
-      author: "Neil Gaiman",
-      url: "https://bookshop.org/p/books/american-gods-neil-gaiman/6438874?ean=9780063081918",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    }
-  ]
   const listWithOneBlog = [
     {
       _id: "5a422aa71b54a676234d17f8",
@@ -69,5 +70,19 @@ describe("total likes", () => {
   test("bigger blog list is calculated correctly", () => {
     const result = listHelper.totalLikes(listWithManyBlogs);
     assert.strictEqual(result, 20)
-  })
+  });
+});
+
+describe("favorite blog", () => {
+  test("blog with the most likes", () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs);
+    assert.deepStrictEqual(
+      result,
+      {
+        title: "A Boy and His Dog at the End of the World",
+        author: "C. a. Fletcher",
+        likes: 7,
+      }
+    );
+  });
 });
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,7 +8,25 @@ const totalLikes = blogPosts => {
   return sum;
 };
 
+const favoriteBlog = blogs => {
+  let favBlog = {
+    title: "",
+    author: "",
+    likes: 0,
+  };
+
+  blogs.map((blog) => {
+    if (blog.likes > favBlog.likes) {
+      favBlog.title = blog.title;
+      favBlog.author = blog.author;
+      favBlog.likes = blog.likes;
+    }
+  });
+  return favBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
