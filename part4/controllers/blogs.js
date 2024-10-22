8383b04ea2ce1abaad113e53badeaa46babc92d8
ravm <miika.ravi@sadeinnovations.com>
2024-10-22 12:52:03 +0300

    4.13-4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -16,4 +16,23 @@ blogsRouter.post("/", async (req, res) => {
   }
 });
 
+blogsRouter.delete("/:id", async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+});
+
+blogsRouter.put("/:id", async(req, res) => {
+  const { title, author, url, likes } = req.body;
+
+  const updatedBlog = {
+    title,
+    author,
+    url,
+    likes,
+  };
+
+  await Blog.findByIdAndUpdate(req.params.id, updatedBlog);
+  res.status(200).end();
+});
+
 module.exports = blogsRouter;
