fa2293414c4092a400d1d002dfbc3a5d3e653133
ravm <miika.ravi@sadeinnovations.com>
2024-10-22 12:52:03 +0300

    4.8

--- part4/app.js
+++ part4/app.js
@@ -13,10 +13,10 @@ logger.info("connecting to", config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
+    console.error("Error connecting to MongoDB:", error.message);
   });
 
 app.use(cors());
---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,20 @@
+const { test, after } = require("node:test");
+const assert = require("node:assert");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+test("application contains one blog that is returned as json", async () => {
+  const response = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  assert.strictEqual(response.body.length, 1);
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
