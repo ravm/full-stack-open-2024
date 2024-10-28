0397bba879d48d46c52f5e14da2705ff421c6483
ravm <miikaravi99@gmail.com>
2024-10-28 11:55:18 +0200

    4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -21,6 +21,26 @@ test("verify unique identifier of blog posts", async () => {
   assert.strictEqual(blog.hasOwnProperty("id"), true);
 });
 
+test("verify successful blog post creation", async () => {
+  const numberOfBlogsBeforePost = (await api.get("/api/blogs")).body.length;
+  
+  const blog = {
+    title: "test POST blog",
+    author: "supertest",
+    url: "http://localhost:3003/api/blogs",
+    likes: 1,
+  };
+
+  const response = await api
+    .post("/api/blogs")
+    .send(blog);
+
+  assert.strictEqual(response.status, 201);
+  
+  const numberOfBlogsAfterPost = (await api.get("/api/blogs")).body.length;
+  assert.strictEqual(numberOfBlogsAfterPost > numberOfBlogsBeforePost, true)
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
