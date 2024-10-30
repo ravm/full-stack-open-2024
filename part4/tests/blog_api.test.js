4cb40cff32b6d1fd16dbdbe36c053e1f1b83457a
ravm <98459176+ravm@users.noreply.github.com>
2024-10-30 08:46:56 +0200

    4.13-4.14 (#4)

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -73,6 +73,46 @@ test("blog post creation without title or url -> bad request", async () => {
     .expect(400);
 });
 
+test("blog post creation without title or url -> bad request", async () => {
+  const blog = {
+    author: "supertest",
+    likes: 2,
+  };
+  
+  await api
+    .post("/api/blogs")
+    .send(blog)
+    .expect(400);
+  });
+
+test("update a single blog post", async () => {
+  const blogsBeforeUpdate = await helper.blogsInDb();
+  const blogId = blogsBeforeUpdate[0].id;
+
+  const updatedBlog = {...blogsBeforeUpdate[0]};
+  updatedBlog.likes = 999;
+
+  await api
+    .put(`/api/blogs/${blogId}`)
+    .send(updatedBlog)
+    .expect(200)
+
+  const blogsAfterUpdate = await helper.blogsInDb();
+  assert.strictEqual(blogsAfterUpdate[0].likes === blogsBeforeUpdate[0].likes, false);
+});
+
+test("delete a single blog post", async () => {
+  const blogs = await helper.blogsInDb();
+  const blogId = blogs[0].id;
+
+  await api
+    .delete(`/api/blogs/${blogId}`)
+    .expect(204);
+  
+  const numberOfBlogsAfterDelete = (await helper.blogsInDb()).length;
+  assert.strictEqual(numberOfBlogsAfterDelete < blogs.length, true);
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
