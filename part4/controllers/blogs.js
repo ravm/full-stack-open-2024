3c9415a805c39d31da5f53fdd93213580e1c8825
ravm <miikaravi99@gmail.com>
2024-11-01 14:13:20 +0200

    4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,18 +1,37 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog
+    .find({}).populate("user", { username: 1, name: 1 })
   res.json(blogs);
 });
 
 blogsRouter.post("/", async (req, res) => {
-  const blog = new Blog(req.body);
+  const { title, author, url, likes } = req.body;
+  let userId = req.body.userId;
+  
+  if (!userId) {
+    const users = await User.find({});
+    userId = users[0]._id;
+  }
 
-  if (!blog.title || !blog.url) {
+  if (!title || !url) {
     res.status(400).end();
   } else {
+    const blog = new Blog({
+      title,
+      author,
+      url,
+      likes,
+      user: userId,
+    });
+
+    const user = await User.findById(userId);
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
     res.status(201).json(savedBlog);
   }
 });
