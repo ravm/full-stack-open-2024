4e16ee7b022e2cfe781c84528dc33cc92cf89bb6
ravm <miikaravi99@gmail.com>
2024-11-14 18:43:09 +0200

    5.1-5.4

---
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const baseUrl = "/api/blogs";
+
+let token = null;
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`;
+}
+
+const getAll = async () => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const res = await axios.get(baseUrl, config);
+  return res.data;
+}
+
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const res = await axios.post(baseUrl, newObject, config);
+  return res.data;
+}
+
+export default { setToken, getAll, create }
