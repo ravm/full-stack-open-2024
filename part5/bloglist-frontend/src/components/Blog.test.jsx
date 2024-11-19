11edc3f08930f8bfe2e5a9704c44cf0e7d6ef872
ravm <miikaravi99@gmail.com>
2024-11-19 11:08:09 +0200

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,11 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { expect, test, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 test("renders only blog's title and author", () => {
   const blog = {
-    id: "123",
     author: "React library",
+    id: "123",
+    likes: 0,
     title: "Content render test",
     url: "www.test.com",
     user: {
@@ -23,4 +25,30 @@ test("renders only blog's title and author", () => {
 
   expect(urlElement).toBeNull();
   expect(likesElement).toBeNull();
+});
+
+test("blog's url and likes are shown when clicking 'Show'", async () => {
+  const blog = {
+    author: "React library",
+    id: "123",
+    likes: 0,
+    title: "URL & likes test",
+    url: "www.test.com",
+    user: {
+      id: "321",
+      username: "testUser",
+    },
+  };
+
+  render(<Blog blog={blog} likeBlog={vi.fn()} deleteBlog={vi.fn()} />);
+
+  const user = userEvent.setup();
+  const showButton = screen.getByText("Show");
+  await user.click(showButton);
+
+  const urlElement = screen.getByText("www.test.com");
+  const likesElement = screen.getByText("Likes: 0");
+
+  expect(urlElement).toBeDefined();
+  expect(likesElement).toBeDefined();
 });
