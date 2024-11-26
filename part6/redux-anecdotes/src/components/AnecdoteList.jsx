cf9f1534739c2380671845013cc6061c4f34b6df
ravm <98459176+ravm@users.noreply.github.com>
2024-11-26 10:44:29 +0200

    6.9 (#13)

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -16,7 +16,12 @@ const Anecdote = ({ anecdote, handleClick }) => {
 }
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state);
+  const anecdotes = useSelector(state => {
+    if (state.filter === "") {
+      return state.anecdotes;
+    }
+    return state.anecdotes.filter(obj => obj.content.toLowerCase().includes(state.filter.toLowerCase()));
+  });
   const dispatch = useDispatch();
 
   return(
