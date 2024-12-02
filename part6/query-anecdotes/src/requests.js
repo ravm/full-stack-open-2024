41d73db97a7c58e84a15294f2c2b4e6bd1278ebd
ravm <miikaravi99@gmail.com>
2024-12-02 11:38:20 +0200

    6.21

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+const baseUrl = "http://localhost:3001/anecdotes";
+
 export const getAnecdotes = async () => {
-  return (await axios.get("http://localhost:3001/anecdotes")).data;
+  return (await axios.get(baseUrl)).data;
+}
+
+export const createAnecdote = async (newAnecdote) => {
+  return (await axios.post(baseUrl, newAnecdote)).data;
 }
