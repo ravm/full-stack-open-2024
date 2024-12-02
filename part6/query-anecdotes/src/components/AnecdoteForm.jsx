2cfa0b4d988845bebe8637407b03765187b43b05
ravm <98459176+ravm@users.noreply.github.com>
2024-12-02 15:07:11 +0200

    6.23-6.24 (#18)

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,13 +1,21 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createAnecdote } from "../requests";
+import { useNotificationDispatch } from "../NotificationContext";
 
 const AnecdoteForm = () => {
+  const dispatch = useNotificationDispatch();
   const queryClient = useQueryClient();
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData(["anecdotes"]);
       queryClient.setQueryData(["anecdotes"], anecdotes.concat(newAnecdote));
+    },
+    onError: () => {
+      dispatch({ type: "SET_NOTIFICATION", payload: "Too short anecdote, must have length of 5 or more" });
+      setTimeout(() => {
+        dispatch({ type: "REMOVE_NOTIFICATION" });
+      }, 5000);
     },
   });
 
@@ -16,6 +24,10 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
     newAnecdoteMutation.mutate({ content, votes: 0 });
+    dispatch({ type: "SET_NOTIFICATION", payload: `Anecdote '${content}' created` });
+    setTimeout(() => {
+      dispatch({ type: "REMOVE_NOTIFICATION" });
+    }, 5000);
 }
 
   return (
